bd9db82ffcff36f7f1d460354e55a2d14d35c426
Scorpion <144194359+Scorpion7162@users.noreply.github.com>
2025-04-27 02:35:51 +0100

    [Chore] Use OAL

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,7 @@ author 'Scorpion'
 repository 'github.com/Scorpion7162/s-throwweapons'
 description 'Throw weapons in GTA V'
 version '1.0.0'
+use_experimental_fxv2_oal 'yes'
 
 client_scripts {
     'client/*.lua',
